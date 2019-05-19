e41434bab3b97a5c17ccfeb0c923f3ab6d0fb76c
Yoz Grahame <yoz@yoz.com>
2019-05-19 00:23:43 -0700

    YOZIFY 3.0: GILBERT EDITION

--- popup.js
+++ popup.js
@@ -20,4 +20,7 @@ function blokeify(bloke) {
 
 function updateIcon(bloke) {
     chrome.browserAction.setIcon({path: bloke + '32.png'})
+    chrome.browserAction.setTitle({
+        title: "Show this to " + bloke.charAt(0).toUpperCase() + bloke.slice(1)
+    })
 }
